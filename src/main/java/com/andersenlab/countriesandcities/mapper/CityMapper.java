adb73f4745af7c38db30472ec75fb08efff5dd22
Dmytro Titov <octopusless@gmail.com>
2023-12-08 09:18:23 +0200

    Update mapper for correct mapping

--- src/main/java/com/andersenlab/countriesandcities/mapper/CityMapper.java
+++ src/main/java/com/andersenlab/countriesandcities/mapper/CityMapper.java
@@ -3,9 +3,11 @@ package com.andersenlab.countriesandcities.mapper;
 import com.andersenlab.countriesandcities.dto.CityDto;
 import com.andersenlab.countriesandcities.model.City;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface CityMapper {
 
+  @Mapping(source = "logoKey", target = "logoUrl")
   CityDto toDto(City city);
 }
